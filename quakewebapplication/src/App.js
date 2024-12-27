0d5f7222792f063a2bc5de012f7e9be350529808
StefanPowell <powellstefan100@gmail.com>
2024-12-27 00:09:00 -0500

    basic design react

--- quakewebapplication/src/App.js
+++ quakewebapplication/src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint react/no-this-in-sfc: 0 */
+import React, { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const MapboxExample = () => {
+  const mapContainerRef = useRef();
+  const mapRef = useRef();
 
-export default App;
+  useEffect(() => {
+    // TO MAKE THE MAP APPEAR YOU MUST
+    // ADD YOUR ACCESS TOKEN FROM
+    // https://account.mapbox.com
+    mapboxgl.accessToken = 'pk.eyJ1Ijoic3RlZmFuMzc1IiwiYSI6ImNtNGhzOXc2dTA3eDYyanBvdGV5dWoya3YifQ.PKTc1k5EspPnZwNBWcEAqw';
+
+    if (mapRef.current) return;
+
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,  // This will ensure the map is mounted to the correct div
+      center: [0, 0],
+      zoom: 2,
+      style: 'mapbox://styles/mapbox/streets-v12'
+    });
+
+    const size = 200;
+
+    const pulsingDot = {
+      width: size,
+      height: size,
+      data: new Uint8Array(size * size * 4),
+
+      onAdd: function () {
+        const canvas = document.createElement('canvas');
+        canvas.width = this.width;
+        canvas.height = this.height;
+        this.context = canvas.getContext('2d');
+      },
+
+      render: function () {
+        const duration = 1000;
+        const t = (performance.now() % duration) / duration;
+
+        const radius = (size / 2) * 0.3;
+        const outerRadius = (size / 2) * 0.7 * t + radius;
+        const context = this.context;
+
+        context.clearRect(0, 0, this.width, this.height);
+        context.beginPath();
+        context.arc(
+          this.width / 2,
+          this.height / 2,
+          outerRadius,
+          0,
+          Math.PI * 2
+        );
+        context.fillStyle = `rgba(255, 200, 200, ${1 - t})`;
+        context.fill();
+
+        context.beginPath();
+        context.arc(this.width / 2, this.height / 2, radius, 0, Math.PI * 2);
+        context.fillStyle = 'rgba(255, 100, 100, 1)';
+        context.strokeStyle = 'white';
+        context.lineWidth = 2 + 4 * (1 - t);
+        context.fill();
+        context.stroke();
+
+        this.data = context.getImageData(0, 0, this.width, this.height).data;
+
+        mapRef.current.triggerRepaint();
+
+        return true;
+      }
+    };
+
+    mapRef.current.on('load', () => {
+      mapRef.current.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
+
+      mapRef.current.addSource('dot-point', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [
+            {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [0, 0]
+              }
+            }
+          ]
+        }
+      });
+
+      mapRef.current.addLayer({
+        id: 'layer-with-pulsing-dot',
+        type: 'symbol',
+        source: 'dot-point',
+        layout: {
+          'icon-image': 'pulsing-dot'
+        }
+      });
+    });
+  }, []);
+
+  return <div id="map" ref={mapContainerRef} style={{ height: '100vh' }}></div>;
+};
+
+export default MapboxExample;
