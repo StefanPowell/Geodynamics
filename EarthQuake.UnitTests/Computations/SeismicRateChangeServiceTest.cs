279be17d8c5790c3016b6d79b65c34675f3873cf
StefanPowell <powellstefan100@gmail.com>
2025-11-13 22:08:32 -0500

    add first basic unit test

---
+++ EarthQuake.UnitTests/Computations/SeismicRateChangeServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using EarthQuake.Computations.Seismology.SeismicRateChange;
+using EarthQuake.Persistence.Repository.Abstractions;
+using Moq;
+using Xunit;
+
+namespace EarthQuake.UnitTests.Computations;
+
+public class SeismicRateChangeServiceTest
+{
+    [Fact]
+    public void GetRateChange_ReturnsCorrectPercentageChange()
+    {
+        //Arrange
+        Mock<IDataQueryRepository> dataQueryRepo = new();
+        SeismicRateChangeService? service = new SeismicRateChangeService(dataQueryRepo.Object);
+        double r1 = 100;   
+        double r2 = 150;   
+
+        double expected = 50;
+
+        //Act
+        double result = service.GetRateChange(r1, r2);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
--- Geodynamics/MigrationScripts/Apply.sql
+++ Geodynamics/MigrationScripts/Apply.sql
@@ -10,54 +10,8 @@ GO
 USE [EarthQuake]
 GO
 
-/****** Object:  Table [dbo].[Feature]    ******/
-SET ANSI_NULLS ON
-GO
+--Normal Tables
 
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[Feature](
-	[FeatureId] [int] IDENTITY(1,1) NOT NULL,
-	[FeatureKey] [nvarchar](100) NOT NULL,
-	[Type] [nvarchar](50) NOT NULL,
-	[PropertiesId] [int] NOT NULL,
-	[GeometryId] [int] NOT NULL,
- CONSTRAINT [PK_Feature] PRIMARY KEY CLUSTERED 
-(
-	[FeatureId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[Feature]  WITH CHECK ADD  CONSTRAINT [FK_Feature_Geometry] FOREIGN KEY([GeometryId])
-REFERENCES [dbo].[Geometry] ([GeometryId])
-GO
-
-ALTER TABLE [dbo].[Feature] CHECK CONSTRAINT [FK_Feature_Geometry]
-GO
-
-ALTER TABLE [dbo].[Feature]  WITH CHECK ADD  CONSTRAINT [FK_Feature_Properties] FOREIGN KEY([PropertiesId])
-REFERENCES [dbo].[Properties] ([PropertiesId])
-GO
-
-ALTER TABLE [dbo].[Feature] CHECK CONSTRAINT [FK_Feature_Properties]
-GO
-
-
-/****** Object:  Table [dbo].[Geometry]    ******/
-CREATE TABLE [dbo].[Geometry](
-	[GeometryId] [int] IDENTITY(1,1) NOT NULL,
-	[type] [nvarchar](50) NOT NULL,
-	[coordinates] [nvarchar](max) NOT NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[GeometryId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-
-/****** Object:  Table [dbo].[Properties]  ******/
 CREATE TABLE [dbo].[Properties](
 	[PropertiesId] [int] IDENTITY(1,1) NOT NULL,
 	[mag] [float] NOT NULL,
@@ -93,7 +47,145 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 
-/****** Object:  UserDefinedTableType [dbo].[Geo]    Script Date: 10/11/2025 11:21:24 PM ******/
+CREATE TABLE [dbo].[Geometry](
+	[GeometryId] [int] IDENTITY(1,1) NOT NULL,
+	[Type] [nvarchar](50) NOT NULL,
+	[Latitude] [real] NOT NULL,
+	[Longitude] [real] NOT NULL,
+	[Depth] [real] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[GeometryId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[Feature](
+	[FeatureId] [int] IDENTITY(1,1) NOT NULL,
+	[FeatureKey] [nvarchar](100) NOT NULL,
+	[Type] [nvarchar](50) NOT NULL,
+	[PropertiesId] [int] NOT NULL,
+	[GeometryId] [int] NOT NULL,
+ CONSTRAINT [PK_Feature] PRIMARY KEY CLUSTERED 
+(
+	[FeatureId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Feature]  WITH CHECK ADD  CONSTRAINT [FK_Feature_Geometry] FOREIGN KEY([GeometryId])
+REFERENCES [dbo].[Geometry] ([GeometryId])
+GO
+
+ALTER TABLE [dbo].[Feature] CHECK CONSTRAINT [FK_Feature_Geometry]
+GO
+
+ALTER TABLE [dbo].[Feature]  WITH CHECK ADD  CONSTRAINT [FK_Feature_Properties] FOREIGN KEY([PropertiesId])
+REFERENCES [dbo].[Properties] ([PropertiesId])
+GO
+
+ALTER TABLE [dbo].[Feature] CHECK CONSTRAINT [FK_Feature_Properties]
+GO
+
+CREATE TABLE [dbo].[faults](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](255) NOT NULL,
+	[type] [nvarchar](50) NOT NULL,
+	[dip] [int] NULL,
+	[last_movement] [date] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[fault_coordinates](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[fault_id] [int] NOT NULL,
+	[latitude] [float] NOT NULL,
+	[longitude] [float] NOT NULL,
+	[point_order] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[fault_coordinates]  WITH CHECK ADD FOREIGN KEY([fault_id])
+REFERENCES [dbo].[faults] ([id])
+ON DELETE CASCADE
+GO
+
+
+CREATE TABLE [dbo].[faultStress](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[faultId] [int] NOT NULL,
+	[faultName] [nvarchar](255) NOT NULL,
+	[slipRate_m_per_yr] [float] NULL,
+	[lockedThickness_m] [float] NULL,
+	[ruptureLength_m] [float] NULL,
+	[shearStressRate_MPa_per_yr] [float] NULL,
+	[accumulatedStress_MPa] [float] NULL,
+	[expectedSlip_m] [float] NULL,
+	[momentMagnitude_Mw] [float] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[faultStress]  WITH CHECK ADD  CONSTRAINT [FK_faultStress_faultId] FOREIGN KEY([faultId])
+REFERENCES [dbo].[faults] ([id])
+GO
+
+
+CREATE TABLE [dbo].[Metadata](
+	[MetadataId] [int] IDENTITY(1,1) NOT NULL,
+	[generated] [bigint] NOT NULL,
+	[url] [nvarchar](2083) NOT NULL,
+	[title] [nvarchar](max) NOT NULL,
+	[status] [int] NOT NULL,
+	[api] [nvarchar](100) NOT NULL,
+	[count] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[MetadataId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+--Table Types
+
+CREATE TYPE [dbo].[Coordinates] AS TABLE(
+	[GeoId] [int] NOT NULL,
+	[PointOrder] [int] NOT NULL,
+	[Coordinate] [float] NOT NULL
+)
+GO
+
+CREATE TYPE [dbo].[FaultStressType] AS TABLE(
+	[faultName] [nvarchar](255) NOT NULL,
+	[slipRate_m_per_yr] [float] NULL,
+	[lockedThickness_m] [float] NULL,
+	[ruptureLength_m] [float] NULL,
+	[shearStressRate_MPa_per_yr] [float] NULL,
+	[accumulatedStress_MPa] [float] NULL,
+	[expectedSlip_m] [float] NULL,
+	[momentMagnitude_Mw] [float] NULL
+)
+GO
+
+CREATE TYPE [dbo].[FeatureType] AS TABLE(
+	[Type] [nvarchar](100) NOT NULL,
+	[PropertiesId] [int] NOT NULL,
+	[GeoId] [int] NOT NULL,
+	[Id] [nvarchar](100) NOT NULL
+)
+GO
+
 CREATE TYPE [dbo].[Geo] AS TABLE(
 	[latitude] [real] NOT NULL,
 	[longitude] [real] NOT NULL,
@@ -102,14 +194,12 @@ CREATE TYPE [dbo].[Geo] AS TABLE(
 )
 GO
 
-/****** Object:  UserDefinedTableType [dbo].[Identifiers]    Script Date: 10/11/2025 11:21:45 PM ******/
 CREATE TYPE [dbo].[Identifiers] AS TABLE(
 	[Id] [nvarchar](100) NOT NULL,
 	[Type] [nvarchar](100) NOT NULL
 )
 GO
 
-/****** Object:  UserDefinedTableType [dbo].[Properties]    Script Date: 10/11/2025 11:21:59 PM ******/
 CREATE TYPE [dbo].[Properties] AS TABLE(
 	[mag] [float] NOT NULL,
 	[place] [nvarchar](255) NOT NULL,
@@ -140,7 +230,30 @@ CREATE TYPE [dbo].[Properties] AS TABLE(
 )
 GO
 
--- stored procedure [InsertFeatures] --
+--Stored Procedures
+
+CREATE PROCEDURE [dbo].[GetLatestFeatures]
+    @TotalValues INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT TOP (@TotalValues)
+        f.FeatureKey AS Id,
+        f.[Type] AS FeatureType,
+        g.[Type] AS GeometryType,
+        g.Latitude,
+        g.Longitude,
+        g.Depth,
+        p.mag AS Magnitude,
+        p.place AS Place,
+        DATEADD(SECOND, p.time / 1000, '1970-01-01') AS QuakeDateTime
+    FROM dbo.Geometry g
+    INNER JOIN dbo.Feature f ON f.GeometryId = g.GeometryId
+    INNER JOIN dbo.Properties p ON f.PropertiesId = p.PropertiesId
+    ORDER BY p.time DESC; 
+END;
+GO
 
 CREATE PROCEDURE [dbo].[InsertFeatures]
     @Identifiers dbo.Identifiers READONLY,
@@ -177,12 +290,14 @@ BEGIN
     -- Insert into Geometry
     INSERT INTO dbo.Geometry
     (
-        [type], [coordinates]
+        [Type], [Latitude], [Longitude], [Depth]
     )
     OUTPUT INSERTED.GeometryId INTO @InsertedGeo(GeometryId)
     SELECT
         g.[type],
-        CONCAT(g.latitude, ',', g.longitude, ',', g.depth)
+		g.[latitude],
+		g.[longitude],
+		g.[depth]
     FROM @Geo g;
 
     -- Insert into Feature (align by row number)
@@ -226,5 +341,29 @@ BEGIN
       AND g.Longitude BETWEEN @longitudeStart AND @longitudeEnd
       AND DATEADD(SECOND, p.time / 1000, '1970-01-01') 
             BETWEEN @startDate AND @endDate;
+END;
+GO
+
+CREATE PROCEDURE [dbo].[UpdateFaultStress]
+    @FaultStressList [dbo].[FaultStressType] READONLY
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    MERGE INTO [dbo].[faultStress] AS target
+    USING @FaultStressList AS source
+    ON target.faultName = source.faultName
+    WHEN MATCHED THEN
+        UPDATE SET
+            slipRate_m_per_yr = source.slipRate_m_per_yr,
+            lockedThickness_m = source.lockedThickness_m,
+            ruptureLength_m = source.ruptureLength_m,
+            shearStressRate_MPa_per_yr = source.shearStressRate_MPa_per_yr,
+            accumulatedStress_MPa = source.accumulatedStress_MPa,
+            expectedSlip_m = source.expectedSlip_m,
+            momentMagnitude_Mw = source.momentMagnitude_Mw
+    WHEN NOT MATCHED BY TARGET THEN
+        INSERT (faultName, slipRate_m_per_yr, lockedThickness_m, ruptureLength_m, shearStressRate_MPa_per_yr, accumulatedStress_MPa, expectedSlip_m, momentMagnitude_Mw)
+        VALUES (source.faultName, source.slipRate_m_per_yr, source.lockedThickness_m, source.ruptureLength_m, source.shearStressRate_MPa_per_yr, source.accumulatedStress_MPa, source.expectedSlip_m, source.momentMagnitude_Mw);
 END;
 GO
