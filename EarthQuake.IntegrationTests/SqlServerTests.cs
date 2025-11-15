ef3b18081453358a4c5e0bf0e693ed96313b9f18
StefanPowell <powellstefan100@gmail.com>
2025-11-14 23:20:51 -0500

    update with integration test

---
+++ EarthQuake.IntegrationTests/SqlServerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Testcontainers.MsSql;
+using Xunit;
+
+namespace EarthQuake.IntegrationTests;
+
+public class SqlServerTests : IAsyncLifetime
+{
+    private readonly MsSqlContainer _db;
+
+    public SqlServerTests()
+    {
+        _db = new MsSqlBuilder()
+            .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+            .WithPassword("Your_password123")     
+            .Build();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _db.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _db.StopAsync();
+    }
+
+    [Fact]
+    public async Task CanConnectToSqlServer()
+    {
+        using var conn = new SqlConnection(_db.GetConnectionString());
+        await conn.OpenAsync();
+
+        Assert.Equal(ConnectionState.Open, conn.State);
+    }
+}
