24500945a33da53194eb461a9726fe7661404dc2
StefanPowell <powellstefan100@gmail.com>
2026-04-11 14:23:53 -0400

    add the stored procedure and the table type for crust stress

--- Geodynamics/MigrationScripts/0005.sql
+++ Geodynamics/MigrationScripts/0005.sql
@@ -40,3 +40,73 @@ CREATE TABLE [dbo].[world_stress_map] (
     PLATE VARCHAR(10) NULL,
     DIST DECIMAL(10,2) NULL
 );
+
+CREATE TYPE [dbo].[CrustStress] AS TABLE(
+	[id] [nvarchar](20) NOT NULL,
+    [isc_id] [nvarchar](20) NULL,
+    [site] [nvarchar](20) NULL,
+    [lat] decimal(8,5) NULL,
+    [lon] decimal(8,5) NULL,
+    [azi] decimal(6,2) NULL,
+    [type] [nvarchar](10) NULL,
+    [depth] decimal(6,2) NULL,
+    [quality] char(1) NULL,
+    [regime] [nvarchar](5) NULL,
+    [locality] [nvarchar](100) NULL,
+    [country] [nvarchar](100) NULL,
+    [date] DATE NULL,
+    [time] TIME NULL,
+    [number] INT NULL,
+    [sd] decimal(6,2) NULL,
+    [tot_len] decimal(10,2) NULL,
+    [vent] [nvarchar](20) NULL,
+    [top] decimal(10,2) NULL,
+    [bot] decimal(10,2) NULL,
+    [anisotropy] [nvarchar](10) NULL,
+    [method] [nvarchar](10) NULL,
+    [s1az] decimal(6,2) NULL,
+    [s1pl] decimal(6,2) NULL,
+    [s2az] decimal(6,2) NULL,
+    [s2pl] decimal(6,2) NULL,
+    [s3az] decimal(6,2) NULL,
+    [s3pl] decimal(6,2) NULL,
+    [mag_type] [nvarchar](10) NULL,
+    [eq_mag] decimal(4,2) NULL,
+    [crust] [nvarchar](20) NULL,
+    [ref1] [nvarchar](20) NULL,
+    [ref2] [nvarchar](20) NULL,
+    [ref3] [nvarchar](20) NULL,
+    [ref4] [nvarchar](20) NULL,
+    [ref5] [nvarchar](20) NULL,
+    [ref6] [nvarchar](20) NULL,
+    [comment] nvarchar(max) NULL,
+    [plate] [nvarchar](10) NULL,
+    [dist] decimal(10,2) NULL
+)
+GO
+
+CREATE PROCEDURE [dbo].[InsertCrustStress]
+    @CrustStress [dbo].[CrustStress] READONLY
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO [dbo].[world_stress_map] (
+    ID, ISC_ID, SITE, LAT, LON, AZI, TYPE, DEPTH, QUALITY, REGIME,
+    LOCALITY, COUNTRY, DATE, TIME, NUMBER, SD, TOT_LEN, VENT, [TOP], BOT,
+    ANISOTROPY, METHOD, S1AZ, S1PL, S2AZ, S2PL, S3AZ, S3PL,
+    MAG_TYPE, EQ_MAG, CRUST,
+    REF1, REF2, REF3, REF4, REF5, REF6,
+    COMMENT, PLATE, DIST
+    )
+    SELECT
+        id, isc_id, site, lat, lon, azi, type, depth, quality, regime,
+        locality, country, date, time, number, sd, tot_len, vent, [top], bot,
+        anisotropy, method, s1az, s1pl, s2az, s2pl, s3az, s3pl,
+        mag_type, eq_mag, crust,
+        ref1, ref2, ref3, ref4, ref5, ref6,
+        comment, plate, dist
+    FROM @CrustStress;
+
+END;
+GO
--- Geodynamics/MigrationScripts/0006.sql
+++ Geodynamics/MigrationScripts/0006.sql
@@ -1,4 +1,7 @@
-DROP TABLE [dbo].[lastProcessedQuake];
+DROP PROCEDURE [dbo].[InsertCrustStress];
+Go
+
+DROP TYPE [dbo].[CrustStress];
 Go
 
 DROP TABLE [dbo].[world_stress_map];
